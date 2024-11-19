0f00af20c74b2ee65cb4d769f7061841d7d9b334
VyTuongVo <vtvo@ucdavis.edu>
2024-11-19 15:09:47 -0800

    done editing the home page except for Readme.md. Evething looks good. all code commented and makes snese

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 This repo is for my CTD Prework assignment. Here I will be building a website using cat API
 
 
+In this project, I used 2 API to make my website. The first API is the imaging API where I used to create "Ramdom picture of the day and the cat gallery". The second used on API was the breed API, used to find The Purrfect Breed by matching user's reseponse to answers from Breed API
+
 The general idea: 
 1. website -> name IDK
 2. Home page, Cat galary. called CAT DUMP! crazy cat pictures
--- main.js
+++ main.js
@@ -1,35 +1,34 @@
+
 const apiKey = 'live_Dz8ZiOyF9uQH8YFNWjm7CWF4fKwlN3W4SKUrwJnKxNoR15wABaQIgPbpGavaJusB'; 
-const imagesUrl = 'https://api.thecatapi.com/v1/images/search?limit=10&size=med'; // Restrict images to medium size
+const imagesUrl = 'https://api.thecatapi.com/v1/images/search?limit=10&size=med'; // in order to not have the picyture being too big, this is the attemp to keep picture size at medium
+
 
 async function fetchCatOfTheDay() {
-    try {
-        const response = await fetch(imagesUrl, {
-            headers: { 'x-api-key': apiKey }
-        });
-        const data = await response.json();
-        displayCatOfTheDay(data[0]);
-    } catch (error) {
-        console.error("Error fetching cat of the day:", error);
-    }
+    const response = await fetch(imagesUrl, {
+        headers: { 'x-api-key': apiKey } 
+    });
+    const data = await response.json();
+    displayCatOfTheDay(data[0]);
 }
 
-// Function to display the image in the HTML
+
 function displayCatOfTheDay(catData) {
-    const catContainer = document.getElementById('cat-of-the-day');
+    const catContainer = document.getElementById('cat-of-the-day');   // connecting with Html to display cat <div id="cat-of-the-day"></div>
     catContainer.innerHTML = `
         <img src="${catData.url}" alt="Random Cat" class="cat-image">
     `;
 }
 
-// Wait for the DOM to load fully before setting up event listeners
+    // working on the refresh button!
 document.addEventListener('DOMContentLoaded', () => {
-    // Load "Cat of the Day" on page load
-    fetchCatOfTheDay();
+    fetchCatOfTheDay();  // will run fetch cat of the day (like a rerun)
 
-    // Add event listener to the refresh button
+        // will run fetch cat of the day (like a rerun) on clicked of the button will vbe creating in html
+
     const refreshButton = document.getElementById('refresh-button');
     refreshButton.addEventListener('click', () => {
         console.log("Refresh button clicked");
         fetchCatOfTheDay();
     });
+
 });
